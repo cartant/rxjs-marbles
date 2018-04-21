3a069d74a46c5a6ed519cdef588b0c302b9227ab
Nicholas Jamieson <nicholas@cartant.com>
2018-04-21 16:14:49 +1000

    chore(examples): Add Tape examples.

---
+++ examples/tape/reframe-spec.ts
@@ -0,0 +1,16 @@
+import { marbles } from "rxjs-marbles/tape";
+import { delay } from "rxjs/operators";
+import * as test from "tape";
+
+test("should support reframing", marbles((m, t) => {
+
+    t.plan(2);
+    m.reframe(100, 10000);
+
+    const duration = m.time("--|");
+    t.assert(duration === 200, "should calculate the duration");
+
+    const source =   m.cold("--(a|)");
+    const expected = m.cold("----(a|)");
+    m.expect(source.pipe(delay(duration, m.scheduler))).toBeObservable(expected);
+}));
---
+++ examples/tape/scheduler-spec.ts
@@ -0,0 +1,45 @@
+import { marbles } from "rxjs-marbles/tape";
+import { asyncScheduler } from "rxjs";
+import { delay } from "rxjs/operators";
+import * as test from "tape";
+
+test("should expose the TestScheduler", marbles((m, t) => {
+
+    t.plan(2);
+
+    const source =  m.hot("--^-a-b-c-|");
+    const subs =            "^---------!";
+    const expected =        "----a-b-c-|";
+
+    const destination = source.pipe(delay(m.time("--|"), m.scheduler));
+    m.expect(destination).toBeObservable(expected);
+    m.expect(source).toHaveSubscriptions(subs);
+}));
+
+test("should support binding specific schedulers", marbles((m, t) => {
+
+    t.plan(2);
+    m.bind(asyncScheduler);
+
+    const source =  m.hot("--^-a-b-c-|");
+    const subs =            "^---------!";
+    const expected =        "----a-b-c-|";
+
+    const destination = source.pipe(delay(m.time("--|"), asyncScheduler));
+    m.expect(destination).toBeObservable(expected);
+    m.expect(source).toHaveSubscriptions(subs);
+}));
+
+test("should support binding all schedulers", marbles((m, t) => {
+
+    t.plan(2);
+    m.bind();
+
+    const source =  m.hot("--^-a-b-c-|");
+    const subs =            "^---------!";
+    const expected =        "----a-b-c-|";
+
+    const destination = source.pipe(delay(m.time("--|")));
+    m.expect(destination).toBeObservable(expected);
+    m.expect(source).toHaveSubscriptions(subs);
+}));
