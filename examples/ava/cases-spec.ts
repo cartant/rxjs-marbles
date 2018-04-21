3e1bea8f416b3f71e84f66337e4b3bff5d405fb7
Nicholas Jamieson <nicholas@cartant.com>
2018-04-21 16:54:45 +1000

    chore(examples): Add AVA examples.

---
+++ examples/ava/cases-spec.ts
@@ -0,0 +1,27 @@
+/*tslint:disable:object-literal-sort-keys*/
+
+import { test } from "ava";
+import { cases, marbles } from "rxjs-marbles/ava";
+import { map } from "rxjs/operators";
+
+cases("should support cases", (m, c, t) => {
+
+    t.plan(1);
+
+    const source =  m.hot(c.s);
+    const expected = m.cold(c.e);
+    const destination = source.pipe(
+        map(value => String.fromCharCode(value.charCodeAt(0) + 1))
+    );
+
+    m.expect(destination).toBeObservable(expected);
+}, {
+    "empty": {
+        s: "-|",
+        e: "-|"
+    },
+    "non-empty": {
+        s: "-a-b-c-|",
+        e: "-b-c-d-|"
+    }
+});
---
+++ examples/ava/reframe-spec.ts
@@ -0,0 +1,16 @@
+import { test } from "ava";
+import { marbles } from "rxjs-marbles/ava";
+import { delay } from "rxjs/operators";
+
+test("should support reframing", marbles((m, t) => {
+
+    t.plan(2);
+    m.reframe(100, 10000);
+
+    const duration = m.time("--|");
+    t.is(duration, 200);
+
+    const source =   m.cold("--(a|)");
+    const expected = m.cold("----(a|)");
+    m.expect(source.pipe(delay(duration, m.scheduler))).toBeObservable(expected);
+}));
