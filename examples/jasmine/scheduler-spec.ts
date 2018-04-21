b9d7f85b9773a61fb0cba9eec06cefa44a7e0053
Nicholas Jamieson <nicholas@cartant.com>
2018-04-21 15:47:07 +1000

    chore(examples): Add Jasmine examples.

---
+++ examples/jasmine/scheduler-spec.ts
@@ -0,0 +1,43 @@
+import { marbles } from "rxjs-marbles/jasmine";
+import { asyncScheduler } from "rxjs";
+import { delay } from "rxjs/operators";
+
+describe("scheduler", () => {
+
+    it("should expose the TestScheduler", marbles(m => {
+
+        const source =  m.hot("--^-a-b-c-|");
+        const subs =            "^---------!";
+        const expected =        "----a-b-c-|";
+
+        const destination = source.pipe(delay(m.time("--|"), m.scheduler));
+        m.expect(destination).toBeObservable(expected);
+        m.expect(source).toHaveSubscriptions(subs);
+    }));
+
+    it("should support binding specific schedulers", marbles(m => {
+
+        m.bind(asyncScheduler);
+
+        const source =  m.hot("--^-a-b-c-|");
+        const subs =            "^---------!";
+        const expected =        "----a-b-c-|";
+
+        const destination = source.pipe(delay(m.time("--|"), asyncScheduler));
+        m.expect(destination).toBeObservable(expected);
+        m.expect(source).toHaveSubscriptions(subs);
+    }));
+
+    it("should support binding all schedulers", marbles(m => {
+
+        m.bind();
+
+        const source =  m.hot("--^-a-b-c-|");
+        const subs =            "^---------!";
+        const expected =        "----a-b-c-|";
+
+        const destination = source.pipe(delay(m.time("--|")));
+        m.expect(destination).toBeObservable(expected);
+        m.expect(source).toHaveSubscriptions(subs);
+    }));
+});
