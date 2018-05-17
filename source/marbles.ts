9af0cd5dd88a0c321d45b0f4541e8e64e0ff1056
Nicholas Jamieson <nicholas@cartant.com>
2018-05-17 10:54:13 +1000

    fix(marbles): Use this context for all funcs.
    That is, for those with a callback parameter, too.

--- source/marbles.ts
+++ source/marbles.ts
@@ -15,8 +15,14 @@ export function marbles<T1, T2, T3>(func: (context: Context, t1: T1, t2: T2, t3:
 export function marbles(func: (context: Context, ...rest: any[]) => any): (...rest: any[]) => any;
 export function marbles(func: (context: Context, ...rest: any[]) => any): (...rest: any[]) => any {
 
+    // Jasmine, Jest and Mocha need to see an explicit parameter for callbacks
+    // to be passed. It's the presence of the parameter that indicates to the
+    // framework that it's an asynchronous test. Not important now, but maybe
+    // later if support for real-world, asynchronous marble tests is to be
+    // added.
+
     if (func.length > 1) {
-        return (first: any, ...rest: any[]) => {
+        return function (this: any, first: any, ...rest: any[]): void {
 
             const scheduler = new TestScheduler((a, b) => observableMatcher(a, b,
                 get("assert"),
@@ -26,7 +32,7 @@ export function marbles(func: (context: Context, ...rest: any[]) => any): (...re
             const context = new Context(scheduler);
 
             try {
-                return func(context, first, ...rest);
+                return func.call(this, context, first, ...rest);
             } finally {
                 context.teardown();
             }
